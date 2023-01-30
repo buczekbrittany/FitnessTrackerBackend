c7be98ec421dbec415a64fa44f2a86144ad9d7f6
Nolimitg7 <gsneed24@gmail.com>
2023-01-30 12:31:20 -0500

    added info to db

--- db/seedData.js
+++ db/seedData.js
@@ -13,12 +13,11 @@ const client = require("./client")
 async function dropTables() {
   try{ 
   console.log("Dropping All Tables...")
-  await client.query
-  // drop all tables, in the correct order
-  (`DROP TABLE IF EXISTS activities;
+  await client.query(`DROP TABLE IF EXISTS activities;
   DROP TABLE IF EXISTS routineActivities;
   DROP TABLE IF EXISTS routine;
   DROP TABLE IF EXISTS users;`)
+  // drop all tables, in the correct order
   }catch(err){
     console.error(err.message);
   }
@@ -27,11 +26,25 @@ async function dropTables() {
 async function createTables() {
   try{
   console.log("Starting to build tables...")
-  // create all tables, in the correct order
-  (`CREATE TABLE activities;
+  await client.query(`CREATE TABLE activities(
+    id	SERIAL	PRIMARY KEY,
+    name	VARCHAR(255)	UNIQUE NOT NULL,
+    description	TEXT	NOT NULL
+  );
   CREATE TABLE routineActivities;
-  CREATE TABLE routine;
-  CREATE TABLE users;`)
+  CREATE TABLE routine(
+    id	SERIAL	PRIMARY KEY,
+    "creatorId"	INTEGER	REFERENCES users(id),
+    "isPublic"	BOOLEAN	DEFAULT false,
+    name	VARCHAR(255)	UNIQUE NOT NULL,
+    goal	TEXT	NOT NULL
+  );
+  CREATE TABLE users(
+    id	SERIAL	PRIMARY KEY,
+    username	VARCHAR(255)	UNIQUE NOT NULL,
+    password	VARCHAR(255)	NOT NULL
+  );`)
+  // create all tables, in the correct order
   }catch(err){
     console.error(err.message);
   }
