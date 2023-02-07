5623791c7b1ee3b9bd38ff9f586d1b725f5dc1c1
buczekbrittany <bbucze2@uic.edu>
2023-02-07 10:32:43 -0800

    getActivityByName test now passing

--- db/activities.js
+++ db/activities.js
@@ -45,7 +45,18 @@ async function getActivityById(id) {
   }
 }
 
-async function getActivityByName(name) {}
+async function getActivityByName(name) {
+  try {
+    const { rows: [ activity ] } = await client.query(`
+    SELECT *
+    FROM activities
+    WHERE name=$1;
+    `, [name]);
+  return activity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {
   // select and return an array of all activities
