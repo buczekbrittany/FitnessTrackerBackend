de0b8dcb7789f5e606c61dc8b8f9696057f780ba
buczekbrittany <bbucze2@uic.edu>
2023-02-07 01:57:02 -0800

    createActivity test passing

--- db/activities.js
+++ db/activities.js
@@ -4,14 +4,13 @@ const client = require('./client');
 
 async function createActivity({ name, description }) {
   // return the new activity
-  try {
-  const { rows: [activity] } = await client.query(`
-    INSERT INTO activities({ name, description })
-    VALUES($1, $2)
-    RETURNING *;
-  `,
-  [ name, description ]
-  );  
+try {
+  const { rows: [ activity ] } = await client.query(`
+  INSERT INTO activities ( name, description )
+  VALUES ($1,$2)
+  ON CONFLICT (name) DO NOTHING
+  RETURNING name, description;
+  `, [ name, description ]);
   return activity;
 } catch (error) {
   throw error;
