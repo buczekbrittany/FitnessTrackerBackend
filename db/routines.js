b0db2fc61d2d98fdfa2191a15caade34b4e674e4
buczekbrittany <bbucze2@uic.edu>
2023-02-08 09:10:37 -0800

    added getRoutineById in routines.js

--- db/routines.js
+++ db/routines.js
@@ -1,23 +1,34 @@
 const client = require("./client");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
+//   try {
+//     const {
+//       rows: [routine],
+//     } = await client.query(
+//       `
+//       INSERT INTO routines ("creatorId", "isPublic", name, goal)
+//       VALUES($1, $2, $3, $4)
+//       RETURNING *;`,
+//       [creatorId, isPublic, name, goal]
+//     );
+//     return routine;
+//   } catch (error) {
+//     throw error;
+//   }
+}
 
+async function getRoutineById(id) {
   try {
-    const { rows: [routine] } = await client.query(`
-    INSERT INTO routines ( creatorId, isPublic, name, goal)
-    VALUES ($1,$2,$3,$4)
-    ON CONFLICT (creatorId) DO NOTHING
-    RETURNING creatorId;
-  
-    `, [ creatorId, isPublic, name, goal]);
+    const { rows: [ routine ] } = await client.query(`
+    SELECT *
+    FROM routines
+    WHERE id=$1;
+    `,[id]);
     return routine;
-    
   } catch (error) {
     throw error;
   }
 }
-
-async function getRoutineById(id) {}
 
 async function getRoutinesWithoutActivities() {}
 
